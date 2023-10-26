7301cca601fa566df23c7d4cac358072bf990850
amanraj5429 <amanrajcgc@gmail.com>
2023-10-27 02:35:41 +0530

    updated code

--- int_concat.py
+++ int_concat.py
@@ -1,6 +1,12 @@
 def int_concat(nums):
-    n = len(nums)
-    sum = 0
-    for i in range(n):
-        sum += nums[i]*(10**(n-i-1))
-    return sum
+
+    if len(nums) == 0:
+        return 0
+    elif len(nums) == 1:
+        return nums[0]
+    else:
+        n = len(nums)
+        sum = 0
+        for i in range(n):
+            sum += nums[i]*(10**(n-i-1))
+        return sum
