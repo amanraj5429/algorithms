7f00cb8fbeba057291aff484e768e33ec9626153
amanraj5429 <amanrajcgc@gmail.com>
2023-11-04 20:39:29 +0530

    code added for no of  vowels in range

---
+++ no_vowel_in_range.py
@@ -0,0 +1,14 @@
+'''
+You are given a 0-indexed array of string words and two integers left and right.
+
+A string is called a vowel string if it starts with a vowel character and ends with a vowel character where vowel characters are 'a', 'e', 'i', 'o', and 'u'.
+
+Return the number of vowel strings words[i] where i belongs to the inclusive range [left, right].
+'''
+def count_vowels(words, left, right):
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    count = 0
+    for i in range(left, right+1):
+        if words[i][0] in vowels and words[i][-1] in vowels:
+            count += 1
+    return count
