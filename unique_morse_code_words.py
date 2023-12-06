ec0ef063604578e65a996358c45fb424181341cf
amanmetapix5429 <Aman.raj@metapix.me>
2023-12-06 11:35:50 +0530

    leet code problem unique_morse_code

---
+++ unique_morse_code_words.py
@@ -0,0 +1,54 @@
+'''
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+'a' maps to ".-",
+'b' maps to "-...",
+'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+Return the number of different transformations among all words we have.
+
+Example 1:
+
+Input: words = ["gin","zen","gig","msg"]
+Output: 2
+Explanation: The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations: "--...-." and "--...--.".
+Example 2:
+
+Input: words = ["a"]
+Output: 1
+'''
+
+def unique_morse_code(words):
+    morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+    chars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    
+    result = []
+    for i in words:
+        res = ''
+        for j in i:
+            if j in chars:
+                ind = chars.index(j)
+                # print(ind)
+                res += morse[ind]
+        result.append(res)
+
+    unique_ele = []
+    for i in result:
+        if i not in unique_ele:
+            unique_ele.append(i)
+    return len(unique_ele)
+
+if __name__ == '__main__':
+    words = ["gin","zen","gig","msg"]
+    ans = unique_morse_code(words)
+    print(ans)
